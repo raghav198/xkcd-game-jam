810387183b52c1e953bb01693086b59858b13ef8
Andrew Davis <pulpdrew@gmail.com>
2017-11-18 00:44:46 -0500

    Initial Commit
    Base project setup

---
+++ desktop/src/com/xkcd/gamejam/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.xkcd.gamejam.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.xkcd.gamejam.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
